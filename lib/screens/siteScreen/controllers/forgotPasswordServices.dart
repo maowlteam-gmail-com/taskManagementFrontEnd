8d74235cfbf3a4649ac1acf6dd42ddcf80a8ca3c
Midhun Murali <midhunmurali776@gmail.com>
2026-01-08 23:18:57 +0530

    Forget Password Fixed

--- lib/screens/siteScreen/controllers/forgotPasswordServices.dart
+++ lib/screens/siteScreen/controllers/forgotPasswordServices.dart
@@ -3,24 +3,28 @@ import 'package:maowl/util/dio_config.dart';
 
 class ForgotPasswordService {
   final dio = DioConfig.getDio();
-  
+
   // Request OTP by sending API request
   // Email is stored in backend, so no need to pass it
   Future<bool> requestOTP() async {
     try {
       final response = await dio.post(
         '${dotenv.env['BASE_URL']}/api/forgot-password',
+        data: {
+          "email": "maowltech@gmail.com",
+        },
         // No need to send email from frontend
         // Backend will use the email associated with the account
       );
-      
+      print("Status Code: "+response.statusCode.toString());
+
       return response.statusCode == 200;
     } catch (e) {
-      print("Error requesting OTP: $e");
+      print("Error requesting OTP: ${e}");
       return false;
     }
   }
-  
+
   // Verify the OTP entered by user
   Future<bool> verifyOTP(String otp) async {
     try {
@@ -31,14 +35,14 @@ class ForgotPasswordService {
           // No need to send email from frontend
         },
       );
-      
+
       return response.statusCode == 200;
     } catch (e) {
       print("Error verifying OTP: $e");
       return false;
     }
   }
-  
+
   // Reset password with new password
   Future<bool> resetPassword(String newPassword) async {
     try {
@@ -49,7 +53,7 @@ class ForgotPasswordService {
           // No need to send email from frontend
         },
       );
-      
+
       return response.statusCode == 200;
     } catch (e) {
       print("Error resetting password: $e");
